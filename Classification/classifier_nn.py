7007d9085c6d14178f539ebfe6e71556552198b4
mifikri <mufti.irawan.f@mail.ugm.ac.id>
2019-02-07 15:26:15 +0700

    add classifier nn

---
+++ Classification/classifier_nn.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+irisClasses = ['setosa','versicolor','virginica']
+
+seed = 7
+np.random.seed(seed)
+
+df = pandas.read_csv("iris.csv")
+X = df.values[:,:4]
+Y = df.values[:,4]
+
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+enY = np_utils.to_categorical(encoded_Y)
+
+xTrain, xTest, yTrain, yTest = train_test_split(X,enY, test_size=0.2, random_state=seed)
+
+model = Sequential()
+model.add(Dense(8, input_dim=X.shape[1], activation='relu'))
+model.add(Dense(3, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+history = model.fit(xTrain,yTrain,validation_data=(xTest,yTest), batch_size=10,epochs=100, verbose=1)
+predictions = np.argmax(model.predict(xTest), axis=1)
+print predictions
+print encoder.inverse_transform(predictions)
+
+plt.plot(history.history['acc'], label='acc')
+plt.plot(history.history['val_acc'], label='val_acc')
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model acc')
+plt.ylabel('acc')
+plt.xlabel('Epoch')
+plt.legend(['Train'], loc='upper left')
+plt.show()
