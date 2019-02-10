68531449ebaee7d13db7479c243bd40c84c10cb4
Mufti Irawan Fikri <31136309+mifikri@users.noreply.github.com>
2019-02-10 10:03:03 +0700

    Update classifier_nn

--- Classification/classifier_nn.py
+++ Classification/classifier_nn.py
@@ -2,10 +2,7 @@ import numpy as np
 import pandas
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
